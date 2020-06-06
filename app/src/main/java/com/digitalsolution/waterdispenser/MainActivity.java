280215e64d180bb63df19d163ab06a1d83a1259f
singj10 <PRiya123>
2020-06-06 13:21:42 +0530

    chnages 2

--- app/src/main/java/com/digitalsolution/waterdispenser/MainActivity.java
+++ app/src/main/java/com/digitalsolution/waterdispenser/MainActivity.java
@@ -2,6 +2,9 @@ package com.digitalsolution.waterdispenser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
@@ -11,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -32,12 +36,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button250;
     private Button button500;
     private Button button750;
+    private Button dispense;
+    private String mTypeWater;
+    private String mQTYWater;
+    WifiManager wifiManager;
+    WifiConfiguration conf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intializeView();
+        connectToWIFI();
 
     }
 
@@ -50,63 +61,123 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button250 = findViewById(R.id.button_250);
         button500 = findViewById(R.id.button_500);
         button750 = findViewById(R.id.button_750);
+        dispense = findViewById(R.id.button_on);
+
         buttonHot.setOnClickListener((View.OnClickListener) this);
         buttonNormal.setOnClickListener((View.OnClickListener) this);
         buttonCold.setOnClickListener((View.OnClickListener) this);
         button250.setOnClickListener((View.OnClickListener) this);
         button500.setOnClickListener((View.OnClickListener) this);
         button750.setOnClickListener((View.OnClickListener) this);
+        dispense.setOnClickListener((View.OnClickListener)this);
+
     }
 
+    private void connectToWIFI() {
+        Toast.makeText(this,"Started..",Toast.LENGTH_SHORT).show();
+        conf = new WifiConfiguration();
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+        //conf.SSID = String.format("\"%s\"", "Jitu");
+        // conf.preSharedKey = String.format("\"%s\"", "Jiten374");
+
+        conf.SSID =  "\""+"Chetan"+"\"";
+        conf.preSharedKey = "\""+"qwerty123"+"\"";
+        Toast.makeText(this,conf.SSID,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,conf.preSharedKey,Toast.LENGTH_SHORT).show();
+
+        conf.status = WifiConfiguration.Status.ENABLED;
+        conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_EAP);
+        //updateNetwork(wifiManager, config);
+
+        int networkId = wifiManager.addNetwork(conf);
+        Toast.makeText(this,conf.status,Toast.LENGTH_SHORT).show();
+        wifiManager.disconnect();
+        Toast.makeText(this,networkId,Toast.LENGTH_SHORT).show();
+        wifiManager.enableNetwork(networkId, true);
+        wifiManager.reconnect();
+        Toast.makeText(this,conf.status,Toast.LENGTH_SHORT).show();
+    }
     public void onClick( View v ){
         if ( v.getId()== R.id.button_hot ) {
             buttonHot.setEnabled(true);
+            mTypeWater= buttonHot.getText().toString();
            // buttonHot.setBackgroundColor(getResources().getColor(R.color.green));
             // do stuff
         }
         else if ( v.getId() == R.id.button_normal ) {
             buttonNormal.setEnabled(true);
+            mTypeWater= buttonNormal.getText().toString();
+
            // buttonNormal.setBackgroundColor(getResources().getColor(R.color.green));
             // do stuff
         }
         else if( v.getId() == R.id.button_cold ) {
             buttonCold.setEnabled(true);
+            mTypeWater= buttonCold.getText().toString();
           //  buttonCold.setBackgroundColor(getResources().getColor(R.color.green));
             // do stuff
         }
-        if ( v.getId() == R.id.button_250 ) {
+        else if ( v.getId() == R.id.button_250 ) {
             button250.setEnabled(true);
+            mQTYWater = button250.getText().toString();
          //   button250.setBackgroundColor(getResources().getColor(R.color.green));
             // do stuff
         }
         else if ( v.getId() == R.id.button_500 ) {
             button500.setEnabled(true);
+            mQTYWater = button500.getText().toString();
          //   button500.setBackgroundColor(getResources().getColor(R.color.green));
             // do stuff
         }
         else if( v.getId() == R.id.button_750 ) {
             button750.setEnabled(true);
+            mQTYWater = button750.getText().toString();
          //   button750.setBackgroundColor(getResources().getColor(R.color.green));
             // do stuff
+        }
+        else if(v.getId() == R.id.button_on){
+            dispense.setBackgroundColor(getResources().getColor(R.color.green));
+            if(mQTYWater != null && mTypeWater != null){
+                try {
+                    SendCommandTOAppliance(mTypeWater,mQTYWater);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
 
-    private void SendCommandTOAppliance() throws UnsupportedEncodingException {
+    private void SendCommandTOAppliance(String type, String qty) throws UnsupportedEncodingException {
+
+        //http://192.168.168.110:8080/dispense?type=hot&qty=250
+
         String data = URLEncoder.encode("cycle", "UTF-8")
                 + "=" + URLEncoder.encode("CycleID", "UTF-8");
 
-        String text = "";
+        String wifiIP ="192.168.168.110:80";
+        String path = "type="+type+"&qty="+qty;
+        String text="";
         BufferedReader reader=null;
         byte[] responseBytes = null;
         try
         {
             // Defined URL  where to send data
-            URL url = new URL("http://" + wifiIP + "/" + "path");
+            URL url = new URL("http://" + wifiIP + "/dispense?" +path);
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-
+            conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "text/plain");
             conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
             conn.setConnectTimeout(5000);
--- app/src/main/java/com/digitalsolution/waterdispenser/ScanQRCode.java
+++ app/src/main/java/com/digitalsolution/waterdispenser/ScanQRCode.java
@@ -1,10 +1,15 @@
 package com.digitalsolution.waterdispenser;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +17,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class ScanQRCode  extends AppCompatActivity {
-    SurfaceView surfaceView;
     Button btnAction;
     private IntentIntegrator mScannerView;
+    WifiManager wifiManager;
+    WifiConfiguration conf;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,96 +34,84 @@ public class ScanQRCode  extends AppCompatActivity {
         initViews();
 
     }
+
     private void initViews() {
-     //   surfaceView = findViewById(R.id.surfaceView);
+        //   surfaceView = findViewById(R.id.surfaceView);
         btnAction = findViewById(R.id.btnAction);
         btnAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mScannerView = new IntentIntegrator(ScanQRCode.this);
-                mScannerView.setOrientationLocked(false);
-                mScannerView.setBeepEnabled(false);
-                mScannerView.initiateScan();
+               // mScannerView = new IntentIntegrator(ScanQRCode.this);
+               // mScannerView.setOrientationLocked(false);
+               // mScannerView.setBeepEnabled(false);
+               // mScannerView.initiateScan();
+                connectToWIFI();
             }
         });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if(intentResult != null) {
-                if (resultCode == RESULT_OK) {
-                    String contents = data.getStringExtra("SCAN_RESULT");
-                    Toast.makeText(this,contents,Toast.LENGTH_SHORT).show();
-                    connectToWIFI();
-                    startActivity(new Intent(ScanQRCode.this, MainActivity.class));
-                }
-                if (resultCode == RESULT_CANCELED) {
-                    Toast.makeText(this,"Scanned failed !!!",Toast.LENGTH_SHORT).show();
-                    connectToWIFI();
-                   // startActivity(new Intent(ScanQRCode.this, MainActivity.class));
-                }
-        }
-        else {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult != null) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Toast.makeText(this, contents, Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ScanQRCode.this, MainActivity.class));
+            }
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Scanned failed !!!", Toast.LENGTH_SHORT).show();
+               // connectToWIFI();
+                startActivity(new Intent(ScanQRCode.this, MainActivity.class));
+            }
+        } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-
-    private void connectToWIFI(){
-        WifiConfiguration conf = new WifiConfiguration();
-        conf.SSID = String.format("\"%s\"", "Chetan");
-        conf.preSharedKey = String.format("\"%s\"", "qwerty123");
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    private void connectToWIFI() {
+        Toast.makeText(this,"Started..",Toast.LENGTH_SHORT).show();
+        conf = new WifiConfiguration();
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         if (!wifiManager.isWifiEnabled()) {
             wifiManager.setWifiEnabled(true);
+        }else{
+            wifiManager.setWifiEnabled(true);
         }
-        int networkId = conf.networkId;
-        wifiManager.disableNetwork(networkId);
+        conf.SSID = String.format("\"%s\"", "Chetan");
+        conf.preSharedKey = String.format("\"%s\"", "qwerty1234");
+
+       // conf.SSID =  "\""+"Chetan"+"\"";
+        //conf.preSharedKey = "\""+"qwerty123"+"\"";
+        Toast.makeText(this,conf.SSID,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,conf.preSharedKey,Toast.LENGTH_SHORT).show();
+
+        conf.status = WifiConfiguration.Status.ENABLED;
+        conf.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        conf.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        conf.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_EAP);
+        //updateNetwork(wifiManager, config);
+
+        int networkId = wifiManager.addNetwork(conf);
+       // Toast.makeText(this,conf.status,Toast.LENGTH_SHORT).show();
         wifiManager.disconnect();
+       // Toast.makeText(this,networkId,Toast.LENGTH_SHORT).show();
         wifiManager.enableNetwork(networkId, true);
         wifiManager.reconnect();
-        }
-
-       private void connectTOWIF(){
-            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            if(!wifiManager.isWifiEnabled())
-            {
-                wifiManager.setWifiEnabled(true);
-            }
-            Method[] wmMethods = wifiManager.getClass().getDeclaredMethods();   //Get all declared methods in WifiManager class
-            boolean methodFound=false;
-            for(Method method: wmMethods){
-                if(method.getName().equals("-get0")){
-                    methodFound=true;
-                    WifiConfiguration netConfig = new WifiConfiguration();
-                    netConfig.SSID = "\""+"Jitu"+"\"";
-                    netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-                    try {
-                     //boolean apstatus=(Boolean) method.invoke(wifiManager, netConfig,true);
-                        for (Method isWifiApEnabledmethod: wmMethods)
-                        {
-                            if(isWifiApEnabledmethod.getName().equals("Jitu")){
-                                while(!(Boolean)isWifiApEnabledmethod.invoke(wifiManager)){
-                                };
-                                for(Method method1: wmMethods){
-                                    if(method1.getName().equals("getWifiApState")){
-                                        int apstate;
-                                        apstate=(Integer)method1.invoke(wifiManager);
-                                    }
-                                }
-                            }
-                        }
-
-                    } catch (IllegalArgumentException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    } catch (InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
+        wifiManager.saveConfiguration();
+       // Toast.makeText(this,conf.status,Toast.LENGTH_SHORT).show();
     }
+
+}
+
+
+
 
